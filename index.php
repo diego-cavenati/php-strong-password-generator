1d7908eaad85b013a8b9576f08116b2d971fd548
diego-cavenati <diegocavenati@gmail.com>
2022-11-29 15:28:54 +0100

    mileston 1 complete - add password generator

--- index.php
+++ index.php
@@ -3,10 +3,42 @@
 $passwordLength = $_GET['passwordLength'];
 $numberLength = (int)$passwordLength;
 
-if (isset($numberLength)) {
-    var_dump($numberLength);
-}
 
+$yourPassword = generatePassword($numberLength);
+
+
+function generatePassword($length)
+{
+    $subLength = ($length / 4);
+    $restLength = ($length % 4);
+    var_dump($subLength);
+    var_dump($restLength);
+
+    $passwordLowCharts = "abcdefghijklmnopqrstuvwxyz";
+    $rndPasswordLowCharts = substr(str_shuffle($passwordLowCharts), 0, $subLength);
+
+    $passwordUpCharts = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $rndPasswordUpCharts = substr(str_shuffle($passwordUpCharts), 0, $subLength);
+
+    $passwordNumber = "0123456789";
+    $rndPasswordNumber = substr(str_shuffle($passwordNumber), 0, $subLength);
+
+    $passwordSymbol = "!#$%&'()*+,-./:;<=>?@[\]^_{|}~";
+    $rndPasswordSymbol = substr(str_shuffle($passwordSymbol), 0, $subLength);
+
+    $passwordnumberRest = "0123456789";
+    $rndPasswordNumberRest = substr(str_shuffle($passwordnumberRest), 0, $restLength);
+
+    if ($restLength > 0) {
+        $passwordFullCharts = $rndPasswordSymbol . $rndPasswordNumber . $rndPasswordUpCharts . $rndPasswordLowCharts . $rndPasswordNumberRest;
+    } else {
+        $passwordFullCharts = $rndPasswordSymbol . $rndPasswordNumber . $rndPasswordUpCharts . $rndPasswordLowCharts;
+    }
+
+    $password = str_shuffle($passwordFullCharts);
+
+    return $password;
+}
 
 ?>
 <!doctype html>
@@ -41,7 +73,7 @@ if (isset($numberLength)) {
                     </div>
                     <button class="btn btn-primary" type="submit">Genera</button>
                 </form>
-
+                <?php echo $yourPassword ?>
             </div>
         </div>
     </main>
